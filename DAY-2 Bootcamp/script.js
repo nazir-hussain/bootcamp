e4dc39e0d121b1572a697cc1d08419d8a433990a
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2024-06-14 21:48:39 +0530

    Update script.js

--- DAY-2 Bootcamp/script.js
+++ DAY-2 Bootcamp/script.js
@@ -1,5 +1,6 @@
 /* Assignment 1:
 Write a JavaScript function that takes a number as a parameter and prints whether it's positive, negative, or zero. */
+// solution
 function checkNumber(number) {
   if (number > 0) {
     console.log("The number is Positive");
